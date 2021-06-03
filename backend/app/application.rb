2a1b2d361460eed45571637ceaf7d28fc45c0a4b
Charles Grant Nichols <grant@inkdots.com>
2021-06-03 11:39:46 -0500

    fixed back end

--- backend/app/application.rb
+++ backend/app/application.rb
@@ -1,12 +1,10 @@
 require 'pry'
 require 'json'
-class Application 
-  
+class Application
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
-
-    if req.path.match(/Users/) 
+    if req.path.match(/Users/)
       if req.post?
         # binding.pry
         rawData = JSON.parse(req.body.read)
@@ -14,28 +12,27 @@ class Application
         return [200, { 'Content-Type' => 'application/json' }, [ newUser.to_json ]]
       end
       return [200, { 'Content-Type' => 'application/json' }, [ {:users => User.all}.to_json ]]
-
     else
       resp.write "Path Not Found"
-
-      if req.path.match(/Results/) 
-        return [200, { 'Content-Type' => 'application/json' }, [ {:results => Result.all}.to_json ]]
-  
+      if req.path.match(/Results/)
+        collection = []
+        Result.all.each do |result|
+          hash = {
+            result: result.result,
+            user_name: result.user.name
+          }
+          collection << hash
+        end
+        return [200, { 'Content-Type' => 'application/json' }, [ {:results => collection}.to_json ]]
       else
         resp.write "Path Not Found"
-
       end
-
-      if req.path.match(/Games/) 
+      if req.path.match(/Games/)
         return [200, { 'Content-Type' => 'application/json' }, [ {:games => Game.all}.to_json ]]
-  
       else
         resp.write "Path Not Found"
       end
-
     end
-
     resp.finish
   end
-
 end
